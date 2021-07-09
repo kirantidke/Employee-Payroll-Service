a28bea011362675f97a8ba6ae7911c9bde860ad6
kirantidke <kirantidke2013@gmail.com>
2021-07-09 16:57:03 +0530

    [kiran]ADDED: functionality to read data and analyse data

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -50,19 +50,32 @@ public class EmployeePayrollService {
 			return EmployeePayrollFileIOService.countEntries();
 		return 0;
 	}
-	
-	
-	    public void printData(IOService ioService){
-	        if (ioService.equals(IOService.FILE_IO)){
-	            new EmployeePayrollFileIOService().printDataFromFile();
-	        }
-	    }
-	   
+
+	/*
+	 * public void printData(IOService ioService){
+	 * if(ioService.equals(IOService.CONSOLE_IO))
+	 * writeEmployeeData(IOService.CONSOLE_IO); else
+	 * if(ioService.equals(IOService.FILE_IO))
+	 * EmployeePayrollFileIOService.printData(); }
+	 */
+	public void printData(IOService ioService) {
+		if (ioService.equals(IOService.FILE_IO)) {
+			new EmployeePayrollFileIOService().printDataFromFile();
+		}
+	}
+
+	public void readDataFromFile(IOService ioService) {
+		if (ioService.equals(IOService.CONSOLE_IO)) {
+			new EmployeePayrollFileIOService().readDataFromFile();
+		}
+	}
+
 	public static void main(String[] args) {
 		ArrayList<EmployeePayrollData> employeePayrollDataList = new ArrayList<EmployeePayrollData>();
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollDataList);
 		Scanner sc = new Scanner(System.in);
 		employeePayrollService.readEmployeePayrollData(sc);
 		employeePayrollService.writeEmployeePayrollData();
+
 	}
 }
